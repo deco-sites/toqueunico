ca9f6553743087d27857a7ae6322e4d9e5a5d3a0
Rafael Maciel <rafael.maciel@meettally.com>
2023-03-09 13:00:50 -0500

    feat: rework on footer

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,66 +1,26 @@
-import FooterAccordion, { sections } from "../components/FooterAccordion.tsx";
-import Icon from "../components/ui/Icon.tsx";
+import { Image as LiveImage } from "$live/std/ui/types/Image.ts";
 
-export default function Footer() {
+export interface Props {
+  imgSrc: LiveImage;
+}
+
+export default function Footer({ imgSrc }: Props) {
   return (
-    <footer class="w-full">
-      <div class="hidden md:block bg-[#353535] w-full">
-        <div class="flex justify-between p-8 w-9/12 mx-auto">
-          {sections.map(({ label, content }) => (
-            <div class="flex flex-col mr-12">
-              <span class="text-white uppercase mb-4 text-xs">{label}</span>
-              {content}
-            </div>
-          ))}
+    <footer class="w-full bg-[#242D21]">
+      <div class="hidden md:block  w-full">
+        <div class="flex justify-between items-center px-[118px] py-[64px] text-[#ECCDA5] text-[24px] font-normal">
+          <p>asdasd</p>
+          <p class="cursor-pointer hover:font-bold">Termos e condições</p>
+          <p class="cursor-pointer hover:font-bold">Privacidade</p>
+          <p class="cursor-pointer hover:font-bold">CNPJ + endereço</p>
         </div>
       </div>
-
-      <div class="md:hidden w-full">
-        <FooterAccordion />
-      </div>
-
-      <div className="flex flex-row flex-col justify-between justify-center items-center px-3 pt-2 pb-5 w-full">
-        <div class="flex flex-col items-center">
-          <ul class="flex items-center justify-center mt-3">
-            <li class="mr-[8px] last:m-0">
-              <a
-                href="#"
-                class="flex items-center justify-center w-8 h-8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Icon id="Facebook" class="w-auto" />
-              </a>
-            </li>
-            <li class="mr-[8px] last:m-0">
-              <a
-                href="https://www.instagram.com/deco.cx"
-                class="flex items-center justify-center w-8 h-8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Icon id="Instagram" class="w-auto" />
-              </a>
-            </li>
-            <li class="mr-[8px] last:m-0">
-              <a
-                href="#"
-                class="flex items-center justify-center w-8 h-8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Icon id="Tiktok" class="w-auto" />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="w-full">
-        <div class="flex justify-center px-3 pb-4">
-          <p class="text-xs text-center">
-            Site by deco.cx
-          </p>
+      <div class="md:hidden pt-[24px] pb-[42px] px-[16px] flex flex-col text-[#ECCDA5] text-[11px] font-normal items-center justify-center">
+        <p class="pb-[32px]">adjasidjiao</p>
+        <div class="flex items-center justify-between w-full">
+          <p class="cursor-pointer hover:font-bold">Termos e condições</p>
+          <p class="cursor-pointer hover:font-bold">Privacidade</p>
+          <p class="cursor-pointer hover:font-bold">CNPJ + endereço</p>
         </div>
       </div>
     </footer>
